d702c9b9c627edec6295fac9d58a02859ec9d238
belostotskaya18 <57672984+belostotskaya18@users.noreply.github.com>
2019-11-19 17:59:00 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Fraction f = new Fraction(5,6);
-        f.multiplication(7,3);
-        f.division(4,2);
-        f.subtraction(3,6);
-        f.addition(3,6);
+        Fraction n = new Fraction(3,5);
+        f.multiplication(n);
+        System.out.println(f.toString());
+        f.division(n);
+        System.out.println(f.toString());
+        f.subtraction(n);
+        System.out.println(f.toString());
+        f.addition(n);
+        System.out.println(f.toString());
+
     }
 }
